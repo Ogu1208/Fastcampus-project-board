1d02c04c482738dc87c8497bff6d529b18ca1904
Ogu1208 <kdasunb@naver.com>
2022-12-25 19:21:53 +0900

    #8 - 도메인 오브젝트 설계 (Article, ArticleComment)
    게시글과 댓글 도메인 오브젝트를 설계함.
    DB와 연동을 위한 추가적인 표현은 추후에 업데이트 예정.

---
+++ src/main/java/com/zerobase/zerobaseprojectboard/domain/Article.java
@@ -0,0 +1,16 @@
+package com.zerobase.zerobaseprojectboard.domain;
+
+import java.time.LocalDateTime;
+
+public class Article {
+    private Long id;
+    private String title;
+    private String content;
+    private String hashtag;
+
+    private LocalDateTime createdAt;
+    private String createdBy;
+    private LocalDateTime modifiedAt;
+    private String modifiedBy;
+
+}
