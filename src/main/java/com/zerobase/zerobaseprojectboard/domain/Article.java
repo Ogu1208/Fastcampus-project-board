9a52904411c16361f29acf72d6a0829e15a6cb1d
Ogu1208 <kdasunb@naver.com>
2022-12-25 19:23:08 +0900

    #8 - 도메인 오브젝트 주석 업데이트
    주석을 업데이트함

--- src/main/java/com/zerobase/zerobaseprojectboard/domain/Article.java
+++ src/main/java/com/zerobase/zerobaseprojectboard/domain/Article.java
@@ -4,13 +4,13 @@ import java.time.LocalDateTime;
 
 public class Article {
     private Long id;
-    private String title;
-    private String content;
-    private String hashtag;
+    private String title; // 제목
+    private String content; // 내용
+    private String hashtag; // 해시태그
 
-    private LocalDateTime createdAt;
-    private String createdBy;
-    private LocalDateTime modifiedAt;
-    private String modifiedBy;
+    private LocalDateTime createdAt; // 생성일시
+    private String createdBy; // 생성자
+    private LocalDateTime modifiedAt; // 수정일시
+    private String modifiedBy; // 수정자
 
 }
--- src/main/java/com/zerobase/zerobaseprojectboard/domain/ArticleComment.java
+++ src/main/java/com/zerobase/zerobaseprojectboard/domain/ArticleComment.java
@@ -4,11 +4,11 @@ import java.time.LocalDateTime;
 
 public class ArticleComment {
     private Long id;
-    private Article article;
-    private String content;
+    private Article article; // 게시글 (ID)
+    private String content; // 내용
 
-    private LocalDateTime createdAt;
-    private String createdBy;
-    private LocalDateTime modifiedAt;
-    private String modifiedBy;
+    private LocalDateTime createdAt; // 생성일시
+    private String createdBy; // 생성자
+    private LocalDateTime modifiedAt; // 수정일시
+    private String modifiedBy; // 수정자
 }
