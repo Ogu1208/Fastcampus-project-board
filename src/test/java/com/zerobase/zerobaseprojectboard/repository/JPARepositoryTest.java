4dc279765eacc78920917dca6164b1257f522ce1
Ogu1208 <kdasunb@naver.com>
2022-12-29 01:25:23 +0900

    #9 - Spring Data JPA Properties와 설정
    Commit을 revert하려다 Stashes를 잘못 삭제해서 관련 파일이 전부 날아가서 애먹음
    -  JPA : auditing  - `JpaConfig` 추가
    -  YML :  DB 관련, 부트 관련 각종 Properties 적용
    -  📌 패스워드 노출 처리
    💥Stashes 만지는거에 주의하자 ㅠㅠㅠ

--- src/test/java/com/zerobase/zerobaseprojectboard/repository/JPARepositoryTest.java
+++ src/test/java/com/zerobase/zerobaseprojectboard/repository/JPARepositoryTest.java
@@ -5,10 +5,8 @@ import com.zerobase.zerobaseprojectboard.domain.Article;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
-import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
 
