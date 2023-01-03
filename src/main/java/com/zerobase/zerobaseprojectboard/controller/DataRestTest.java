daa147b43428757575e93cf48a1ad4ca98306907
Ogu1208 <kdasunb@naver.com>
2023-01-04 01:16:03 +0900

    WebMVC 인식 안돼서 잠시 커밋

---
+++ src/main/java/com/zerobase/zerobaseprojectboard/controller/DataRestTest.java
@@ -0,0 +1,5 @@
+package com.zerobase.zerobaseprojectboard.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+public class DataRestTest {
+}
--- src/main/java/com/zerobase/zerobaseprojectboard/repository/ArticleCommentRepository.java
+++ src/main/java/com/zerobase/zerobaseprojectboard/repository/ArticleCommentRepository.java
@@ -2,6 +2,8 @@ package com.zerobase.zerobaseprojectboard.repository;
 
 import com.zerobase.zerobaseprojectboard.domain.ArticleComment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
 }
