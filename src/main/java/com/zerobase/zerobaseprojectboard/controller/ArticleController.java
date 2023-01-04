e4268cbb07b1eef1d2e292f294a91e09ffb9db33
Ogu1208 <kdasunb@naver.com>
2023-01-05 02:41:21 +0900

    #11 - 게시글 뷰 html 골격 작성
    Controller, view, test 한번에 적용

--- src/main/java/com/zerobase/zerobaseprojectboard/controller/ArticleController.java
+++ src/main/java/com/zerobase/zerobaseprojectboard/controller/ArticleController.java
@@ -3,6 +3,7 @@ package com.zerobase.zerobaseprojectboard.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -22,5 +23,12 @@ public class ArticleController {
     public String articles(ModelMap map) {
         map.addAttribute("articles", List.of());
         return "articles/index";
+    }
+
+    @GetMapping("/{articleId}")
+    public String article(@PathVariable Long articleId, ModelMap map) {
+        map.addAttribute("article", "article");  //TODO: 구현할 때 실제 데이터 넣어야함
+        map.addAttribute("articleComments", List.of());
+        return "articles/detail";
     }
 }
--- src/test/java/com/zerobase/zerobaseprojectboard/controller/ArticleControllerTest.java
+++ src/test/java/com/zerobase/zerobaseprojectboard/controller/ArticleControllerTest.java
@@ -38,7 +38,7 @@ class ArticleControllerTest {
 
     }
 
-    @Disabled("구현 중")
+//    @Disabled("구현 중")
     @DisplayName("[view][GET] 게시글 상세 페이지 - 정상 호출")
     @Test
     public void givenNothing_whenRequestArticleView_thenReturnsArticleView() throws Exception {
