a632bde259d4e21c49f23cb5f5904ea506bb7f95
Ogu1208 <kdasunb@naver.com>
2023-01-05 00:55:10 +0900

    #11 - 게시판 뷰 html 골격 작성
    컨트롤러 핸들러 메소드로 테스트를 통과하게 열어줌
    뷰 html 뼈대 작성
    테스트에서는 media type 검사에서 부가적인 옵션이 통과되도록 테스트 수정

--- src/main/java/com/zerobase/zerobaseprojectboard/controller/ArticleController.java
+++ src/main/java/com/zerobase/zerobaseprojectboard/controller/ArticleController.java
@@ -1,7 +1,11 @@
 package com.zerobase.zerobaseprojectboard.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 /**
  * /articles
@@ -13,4 +17,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/articles")
 @Controller
 public class ArticleController {
+
+    @GetMapping
+    public String articles(ModelMap map) {
+        map.addAttribute("articles", List.of());
+        return "articles/index";
+    }
 }
--- src/test/java/com/zerobase/zerobaseprojectboard/controller/ArticleControllerTest.java
+++ src/test/java/com/zerobase/zerobaseprojectboard/controller/ArticleControllerTest.java
@@ -23,7 +23,7 @@ class ArticleControllerTest {
         this.mvc = mvc;
     }
 
-    @Disabled("구현 중")
+//    @Disabled("구현 중")
     @DisplayName("[view][GET] 게시글 리스트 (게시판) - 정상 호출")
     @Test
     public void givenNothing_whenRequestArticlesView_thenReturnsArticlesView() throws Exception {
@@ -32,7 +32,7 @@ class ArticleControllerTest {
         //when & then
         mvc.perform(get("/articles"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))  // view -> text_html
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))  // view -> text_html
                 .andExpect(view().name("articles/index"))  // view 파일 이름 검사
                 .andExpect(model().attributeExists("articles"));  // view에 model_attirute로 articles를 내려준 것을 확인
 
