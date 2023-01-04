77928647d83cb1508c23b0e223e2812e9e9eac6f
Ogu1208 <kdasunb@naver.com>
2023-01-04 14:32:38 +0900

    #21 - Spring Data REST + HAL Explorer 디펜던시 추가, Data REST 설정
     필요한 설정 추가, 게시글, 댓글의 json API를 자동으로 restful하게 만들도록 설정

--- src/main/java/com/zerobase/zerobaseprojectboard/repository/ArticleCommentRepository.java
+++ src/main/java/com/zerobase/zerobaseprojectboard/repository/ArticleCommentRepository.java
@@ -4,6 +4,6 @@ import com.zerobase.zerobaseprojectboard.domain.ArticleComment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource
+@RepositoryRestResource  // Spring Data REST
 public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
 }
--- src/main/java/com/zerobase/zerobaseprojectboard/repository/ArticleRepository.java
+++ src/main/java/com/zerobase/zerobaseprojectboard/repository/ArticleRepository.java
@@ -4,6 +4,6 @@ import com.zerobase.zerobaseprojectboard.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource
+@RepositoryRestResource  // Spring Data REST
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 }
