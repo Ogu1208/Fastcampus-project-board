8eba9c1eb09caf76a0d11b7d2c5a3a25292e131d
Ogu1208 <kdasunb@naver.com>
2023-01-04 23:13:09 +0900

    #11 - 게시글 관련 뷰 테스트 작성
    어떤 경로로 뷰를 열어줄지 테스트로 명세서 작성

---
+++ src/main/java/com/zerobase/zerobaseprojectboard/controller/ArticleController.java
@@ -0,0 +1,16 @@
+package com.zerobase.zerobaseprojectboard.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * /articles
+ * /articles/{article-id}
+ * /articles/search
+ * /articles/search-hashtag
+ */
+
+@RequestMapping("/articles")
+@Controller
+public class ArticleController {
+}
---
+++ src/test/java/com/zerobase/zerobaseprojectboard/controller/ArticleControllerTest.java
@@ -0,0 +1,76 @@
+package com.zerobase.zerobaseprojectboard.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@DisplayName("View 컨트롤러 - 게시글")
+@WebMvcTest(ArticleController.class)
+class ArticleControllerTest {
+    private final MockMvc mvc;
+
+    public ArticleControllerTest(@Autowired MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+    @DisplayName("[view][GET] 게시글 리스트 (게시판) - 정상 호출")
+    @Test
+    public void givenNothing_whenRequestArticlesView_thenReturnsArticlesView() throws Exception {
+        // given
+
+        //when & then
+        mvc.perform(get("/articles"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML))  // view -> text_html
+                .andExpect(model().attributeExists("articles"));  // view에 model_attirute로 articles를 내려준 것을 확인
+
+    }
+
+    @DisplayName("[view][GET] 게시글 상세 페이지 - 정상 호출")
+    @Test
+    public void givenNothing_whenRequestArticleView_thenReturnsArticleView() throws Exception {
+        // given
+
+        //when & then
+        mvc.perform(get("/articles/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML))  // view -> text_html
+                .andExpect(model().attributeExists("article"));  // view에 model_attirute로 articles를 내려준 것을 확인
+
+    }
+
+    @DisplayName("[view][GET] 게시글 검색 전용 페이지 - 정상 호출")
+    @Test
+    public void givenNothing_whenRequestArticleSearchView_thenReturnsArticleSearchView() throws Exception {
+        // given
+
+        //when & then
+        mvc.perform(get("/articles/search"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML)); // view -> text_html
+
+
+    }
+
+    @DisplayName("[view][GET] 게시글 해시태그 검색 페이지 - 정상 호출")
+    @Test
+    public void givenNothing_whenRequestArticleHashtagSearchView_thenReturnsArticleHashtagSearchView() throws Exception {
+        // given
+
+        //when & then
+        mvc.perform(get("/articles/search-hashtag"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML));// view -> text_html
+
+
+    }
+}
