de2e42911763638c3e0695ed73e2e00df49acf5e
Ogu1208 <kdasunb@naver.com>
2023-01-08 03:53:41 +0900

    #11 - 로그인 컨트롤러 테스트 + 시큐리티 설정을 컨트롤러 테스트에 반영
    스프링 시큐리티가 자동 생성한  `/login` 컨트롤러를 테스트함
    모든 컨트롤러가 Security Config를 부를 수 있도록 `@import` 추가

--- src/test/java/com/zerobase/zerobaseprojectboard/controller/ArticleControllerTest.java
+++ src/test/java/com/zerobase/zerobaseprojectboard/controller/ArticleControllerTest.java
@@ -1,10 +1,12 @@
 package com.zerobase.zerobaseprojectboard.controller;
 
+import com.zerobase.zerobaseprojectboard.config.SecurityConfig;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -15,6 +17,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @DisplayName("View 컨트롤러 - 게시글")
+@Import(SecurityConfig.class)
 @WebMvcTest(ArticleController.class)
 class ArticleControllerTest {
     private final MockMvc mvc;
@@ -22,6 +25,7 @@ class ArticleControllerTest {
     public ArticleControllerTest(@Autowired MockMvc mvc) {
         this.mvc = mvc;
     }
+
 
 //    @Disabled("구현 중")
     @DisplayName("[view][GET] 게시글 리스트 (게시판) - 정상 호출")
