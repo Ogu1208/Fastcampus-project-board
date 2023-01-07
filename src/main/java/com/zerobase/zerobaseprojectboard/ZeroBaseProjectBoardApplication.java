e80a91f151144ca25e1e14a526b631504df84538
Ogu1208 <kdasunb@naver.com>
2023-01-08 03:55:35 +0900

    #11 - Application에서 잘못된 어노테이션 수정
    Thymeleaf 사용자 설정 config를 반영하게 하기 위한 어노테이션을 잘못 작성함.
    `@ConfigurationPropertiesScan`로 변경

--- src/main/java/com/zerobase/zerobaseprojectboard/ZeroBaseProjectBoardApplication.java
+++ src/main/java/com/zerobase/zerobaseprojectboard/ZeroBaseProjectBoardApplication.java
@@ -2,7 +2,9 @@ package com.zerobase.zerobaseprojectboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Configuration;
 
 @ConfigurationPropertiesScan
 @SpringBootApplication
