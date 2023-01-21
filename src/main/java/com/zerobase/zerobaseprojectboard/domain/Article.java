f9fc870d520220e6cc1ffdbdf3e04b1a3a135a00
Ogu1208 <kdasunb@naver.com>
2023-01-21 23:33:24 +0900

    #12 - UserAccount 엔티티와 Article, ArticleComment에연관관계 적용
    테이블 필드 수정으로 영향 받은
    `data.sql` 테스트 데이터도 모두 업데이트
    
    -  게시글 -> 댓글 컬렉션의 정렬을 최신 순으로 적용

--- src/main/java/com/zerobase/zerobaseprojectboard/domain/Article.java
+++ src/main/java/com/zerobase/zerobaseprojectboard/domain/Article.java
@@ -10,7 +10,7 @@ import java.util.Objects;
 import java.util.Set;
 
 @Getter
-@ToString
+@ToString(callSuper = true)
 @Table(indexes = {
         @Index(columnList = "title"),
         @Index(columnList = "hashtag"),
@@ -23,6 +23,8 @@ public class Article extends AuditingFields{
     @GeneratedValue(strategy = GenerationType.IDENTITY) // mysql의 autoincreasement는 IDENTITY
     private Long id;
 
+    @Setter @ManyToOne(optional = false) private UserAccount userAccount; // 유저 정보 (ID)
+
     @Setter @Column(nullable = false) private String title; // 제목
     @Setter @Column(nullable = false, length = 10000) private String content; // 내용
 
@@ -31,7 +33,7 @@ public class Article extends AuditingFields{
     private String hashtag; // 해시태그
 
     // One to Many - Coment들을 중복을 허용하지 않고 모아서 보여줌
-    @OrderBy("id")
+    @OrderBy("createdAt DESC")
     @OneToMany(mappedBy = "article", cascade = CascadeType.ALL)
     @ToString.Exclude
     private final Set<ArticleComment> articleComments = new LinkedHashSet<>();
@@ -40,14 +42,15 @@ public class Article extends AuditingFields{
     protected Article() {
     }
 
-    private Article(String title, String content, String hashtag) {
+    private Article(UserAccount userAccount, String title, String content, String hashtag) {
+        this.userAccount = userAccount;
         this.title = title;
         this.content = content;
         this.hashtag = hashtag;
     }
 
-    public static Article of(String title, String content, String hashtag) {
-        return new Article(title, content, hashtag);
+    public static Article of(UserAccount userAccount, String title, String content, String hashtag) {
+        return new Article(userAccount, title, content, hashtag);
     }
 
     @Override
