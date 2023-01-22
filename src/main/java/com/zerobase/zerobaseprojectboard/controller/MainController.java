a9e48310c446b8b5ee74bdf06c0beee2c7c8bb9f
Ogu1208 <kdasunb@naver.com>
2023-01-23 00:09:10 +0900

    #12 - 루트 경로를 게시판 페이지로 forward 하도록 컨트롤러 구현
    내용 수정

--- src/main/java/com/zerobase/zerobaseprojectboard/controller/MainController.java
+++ src/main/java/com/zerobase/zerobaseprojectboard/controller/MainController.java
@@ -8,6 +8,6 @@ public class MainController {
 
     @GetMapping("/")
     public String root() {
-        return "redirect:/articles";
+        return "forward:/articles";
     }
 }
--- src/test/java/com/zerobase/zerobaseprojectboard/controller/MainControllerTest.java
+++ src/test/java/com/zerobase/zerobaseprojectboard/controller/MainControllerTest.java
@@ -6,9 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @Import(SecurityConfig.class)
 @WebMvcTest(MainController.class)
@@ -26,6 +27,10 @@ class MainControllerTest {
         // given
         // when & then
         mvc.perform(get("/"))
-                .andExpect(status().is3xxRedirection());
+                .andExpect(status().isOk())
+                .andExpect(view().name("forward:/articles"))
+                .andExpect(forwardedUrl("/articles"))
+                .andDo(MockMvcResultHandlers.print());
     }
 }
+
